b55bd781f8b62b3067e9a2eb605c6747cc416683
chuaziheng <56360597+chuaziheng@users.noreply.github.com>
2019-10-26 19:15:28 +0800

    Update mainprogram.py

--- mainprogram.py
+++ mainprogram.py
@@ -1,23 +1,36 @@
 import calendar
 from datetime import date
+from datetime import datetime
 
 d = date.today()
 day_of_week = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 list_of_all_stall = ["McDonald", "The Sandwich Guys", "Subway", "Mini Wok", "Malay Food"]
-dictionary = {'Monday' : ["Mini Wok","McDonald", "Malay Food", "Ma La Xiang Guo"],
-                'Tuesday' : ["The Sandwich Guys", "McDonald", "Subway", "Cantonese"],
-                'Wednesday' : ["McDonald", "Subway", "Long John"],
-                'Thursday' : ["Japanese","McDonald","Chicken Rice","Malay Food"],
+dictionary = {'Monday' : ["Mini Wok","McDonald Breakfast","McDonald Lunch", "Malay Food", "Ma La Xiang Guo"],
+                'Tuesday' : ["The Sandwich Guys", "McDonald Breakfast","McDonald Lunch", "Subway", "Cantonese"],
+                'Wednesday' : ["McDonald Breakfast","McDonald Lunch", "Subway", "Long John"],
+                'Thursday' : ["Japanese","McDonald Breakfast","McDonald Lunch","Chicken Rice","Malay Food"],
                 'Friday' : ["Mini Wok","Subway","Long John","Starbucks"],
-                'Saturday' : ["Indian","Japanese","McDonald","Mini Wok"],
-                'Sunday' : ["McDonald","Subway","Long John"]}
+                'Saturday' : ["Indian","Japanese","McDonald Breakfast","McDonald Lunch","Mini Wok"],
+                'Sunday' : ["McDonald Breakfast","McDonald Lunch","Subway","Long John"]}
 operate_hour ={'McDonald' : "08.00-24.00",'Mini Wok' : "09.00-20.00", 'Malay Food' :"10.00-20.00",
                'Subway' : "09.00-20.00", 'The Sandwich Guys' : "08.00-19.00"}
+menu_hours = {'McDonald Breakfast' : "08.00-11.00",'McDonald Lunch': "11.00-23.59",'Mini Wok' : "09.00-20.00", 
+              'Malay Food' :"10.00-20.00",'Subway' : "09.00-20.00", 'The Sandwich Guys' : "08.00-19.00"}
 
+#Find Date and Time
+def find_datetime():
+  now = datetime.now()
+  # dd/mm/YY H:M:S
+  dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+  t_string = now.strftime( "%H:%M:%S")
+  return print(dt_string)
+  
 #Display the Main Menu
 def print_menu():
     choice = 0
     while choice < 1 or choice > 5:
+        find_datetime()
+        print("\n")
         print("Welcome to Canteen A North Spine Information System!")
         print("\n")
         print("Menu : \n1. Stall Info \n2. Current Stalls \n3. Check Stalls on Date\n4. Operating Hours\n5. Check Queue")
@@ -43,13 +56,13 @@ def display_menu1():
     choice1 = int(input("Enter which stall do you want :"))
     display_stall(list_of_all_stall[choice1-1])
 
-#Find day
+#Find day of the week
 def findDay(day, month, year) :     
     dayNumber = calendar.weekday(year, month, day)
     thatDay = day_of_week[dayNumber]
     return thatDay
 
-#Print Stall Based on Day
+#Print Stall Based on Day and Time (Function C)
 def print_stall_day(day) :
     list_stall_that_day = dictionary[day]
     for i in range(len(list_stall_that_day)) :
